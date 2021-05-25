8d744fd90dc65cb7585b2327a10049e478d66f44
jimosk <jimosdk@gmail.com>
2021-05-25 19:54:28 +0300

    Implemented Player class and Player#make_move method for making a single move on the board

---
+++ player.rb
@@ -0,0 +1,40 @@
+require_relative 'display.rb'
+
+class Player
+    attr_reader :color 
+    def initialize(color , display)
+        @color = color
+        @display = display
+    end
+
+end
+
+class HumanPlayer < Player
+
+    def make_move(board)
+        move = []
+        loop do
+            @display.render
+            input = @display.cursor.get_input(@color)
+            move << input unless input.nil?
+            if move.length == 2
+                begin
+                    board.move_piece(move.first,move.last)
+                    break
+                rescue => e  
+                puts e.message
+                sleep(1.5)
+                ensure
+                    move = []
+                end
+            end 
+        end
+    end
+end
+
+class ComputerPlayer < Player
+    def make_move(board)
+    end
+end
+
+
